3edb492905589fe64cf9d8b981be6d4f5db8d88c
Liu Jiaming <storypku@gmail.com>
2013-11-27 12:24:13 +0800

    TCP_SERV_POLL_01: EADDRINUSE; Decrement maxi

--- unpv1-dist/tcpcliserv/tcpservpoll01.c
+++ unpv1-dist/tcpcliserv/tcpservpoll01.c
@@ -1,15 +1,31 @@
+/* Modified version by Liu Jiaming
+ * 1) Resolves the "Address already in use" problem
+ * 2) Decrement maxi when client closes/aborts the connection
+ */
 /* include fig01 */
 #include	"unp.h"
 #include	<limits.h>		/* for OPEN_MAX */
 #ifndef OPEN_MAX	/* It seems that OPEN_MAX was obsolete on Linux */
 #define OPEN_MAX 1024
 #endif
+static void decrement_maxi(struct pollfd *client, int *maxi) {
+	int j, pos;
+	pos = *maxi;
+	for(j = pos; j >= 0; j--) {
+		if(client[pos].fd == -1)
+			pos--;
+		else
+			break;
+	}
+	*maxi = pos;
+}
 
 int
 main(int argc, char **argv)
 {
 	int					i, maxi, listenfd, connfd, sockfd;
 	int					nready;
+	int 				optval;
 	ssize_t				n;
 	char				buf[MAXLINE];
 	socklen_t			clilen;
@@ -22,6 +38,11 @@ main(int argc, char **argv)
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(SERV_PORT);
+
+	optval = 1;
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &optval,
+							sizeof(optval)) == -1)
+		err_sys("setsockopt-SOL_SOCKET-SO_REUSEADDR");
 
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
@@ -73,6 +94,7 @@ main(int argc, char **argv)
 #endif
 						Close(sockfd);
 						client[i].fd = -1;
+						decrement_maxi(client, &maxi);
 					} else
 						err_sys("read error");
 				} else if (n == 0) {
@@ -82,6 +104,7 @@ main(int argc, char **argv)
 #endif
 					Close(sockfd);
 					client[i].fd = -1;
+					decrement_maxi(client, &maxi);
 				} else
 					Writen(sockfd, buf, n);
 
