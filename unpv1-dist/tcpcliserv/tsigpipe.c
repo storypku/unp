44dcae680b618614e0afe013d38b9f8a02e6fc50
Liu Jiaming <storypku@gmail.com>
2013-12-04 15:15:22 +0800

    Exercise 5.6

--- unpv1-dist/tcpcliserv/tsigpipe.c
+++ unpv1-dist/tcpcliserv/tsigpipe.c
@@ -1,5 +1,19 @@
+/* File tcpcliserv/tsigpipe.c */
 #include	"unp.h"
-
+/* Running this program under Ubuntu 13.10 x86_64 generates the following:
+ *
+ * 		[story@ubuntu:unp]$ tsigpipe 127.0.0.1
+ * 		SIGPIPE received
+ * 		write error: [EPIPE] Broken pipe
+ *
+ * The initial sleep of 2s is to let the daytime server send its reply and
+ * close its end of connection. Our first write sends a data segment to the
+ * server, which responds with an RST (since the daytime server has completely
+ * closed its socket). Note that our TCP allows us to write to a socket that 
+ * has received a FIN. The second sleep lets the server's RST be received, and
+ * our second write generates SIGEPIPE. Since our signal handler returns,
+ * write returns an error of EPIPE.
+ */
 void
 sig_pipe(int signo)
 {
