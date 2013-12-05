12be6135502a0a1492cac15b3e28e12ebcf04a52
Liu Jiaming <storypku@gmail.com>
2013-12-05 19:07:19 +0800

    Exercise 7.4

--- Exercises/Ex7.4/Ex7.4.tcp_cli.c
+++ Exercises/Ex7.4/Ex7.4.tcp_cli.c
@@ -1,4 +1,26 @@
-/* Use standard echo server; baseline measurements for nonblocking version */
+/* Note:
+ * The race condition (described in UNPv1's solution for exercise 7.4)
+ * cause bind() to return EADDRINUSE on MAC OS X 10.9, while make connect()
+ * to return EADDRNOTAVAIL on Ubuntu Linux 13.10 x86_64.
+ *
+ * # Start the TCP echo server on host 10.1.81.55 and ports 7000, 8000
+ * $./serv 7000 &
+ * $./serv 8000 &
+ *
+ * # The client bind to 10.1.81.51 and 1500 before connect().
+ *
+ * $tty
+ * /dev/pts/0
+ * $./cli 10.1.81.51 1500 10.1.81.55 7000
+ *
+ * $tty
+ * /dev/pts/5
+ * $./cli 10.1.81.51 1500 10.1.81.55 8000
+ *
+ * However, if the tuple (local-addr, local-port, foreign-addr, foreign-port)
+ * is not unique, then on Mac OS X 10.9, connect() will return EADDRINUSE;
+ * 
+ */
 #include	"unp.h"
 
 int
@@ -18,17 +40,27 @@ main(int argc, char **argv)
 	cliaddr.sin_family = AF_INET;
 	cliaddr.sin_port = htons((short int)atoi(argv[2]));
 	Inet_pton(AF_INET, argv[1], &cliaddr.sin_addr);
-	Bind(sockfd, (const SA *)&cliaddr, sizeof(cliaddr));
 
-	sleep(10); /* Sleep to let another instance to bind */
+#ifdef __LINUX__
+	Bind(sockfd, (const SA *)&cliaddr, sizeof(cliaddr));
+#else
+	while(bind(sockfd, (const SA *)&cliaddr, sizeof(cliaddr)) < 0 && errno == EADDRINUSE)
+		continue;
+#endif
+
+	sleep(10); /* Sleep to give another instance to run and call bind() */
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons((short int)atoi(argv[4]));
 	Inet_pton(AF_INET, argv[3], &servaddr.sin_addr);
 
-	while(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0)
+#ifdef __LINUX__
+	while(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0 && errno == EADDRNOTAVAIL)
 		continue;
+#else
+	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+#endif
 	str_cli(stdin, sockfd);
 	exit(0);
 }
