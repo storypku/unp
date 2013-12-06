9a89410dd7283aa43b04f42d458bd2a219872811
Liu Jiaming <storypku@gmail.com>
2013-12-06 18:22:32 +0800

    memcmp sockaddr_in doesn't work correctly on Mac OS X 10.9

--- unpv1-dist/udpcliserv/dgcliaddr.c
+++ unpv1-dist/udpcliserv/dgcliaddr.c
@@ -16,7 +16,19 @@ dg_cli(FILE *fp, int sockfd, const SA *pservaddr, socklen_t servlen)
 
 		len = servlen;
 		n = Recvfrom(sockfd, recvline, MAXLINE, 0, preply_addr, &len);
-		if (len != servlen || memcmp(pservaddr, preply_addr, len) != 0) {
+
+		/* Liu Jiaming's Note:	Dec 06, 2013
+		 * As tested on Mac OS X 10.9, even if the (addr, port) tuple for
+		 * pservaddr and preply_addr does match, memcmp returns a non-zero
+		 * value, indicating that additional field(s) has been filled from
+		 * the recvfrom() call by the kernel. So, for maximum portability,
+		 * I use the sock_cmp_addr() and sock_cmp_port() functions to verify
+		 * returned socket address/port.
+		 * */
+
+		/* if (len != servlen || memcmp(pservaddr, preply_addr, len) != 0) { */
+		if (len != servlen || sock_cmp_addr(pservaddr, preply_addr, len) != 0
+						   || sock_cmp_port(pservaddr, preply_addr, len) != 0) {
 			printf("reply from %s (ignored)\n",
 					Sock_ntop(preply_addr, len));
 			continue;
